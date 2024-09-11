a82a7377551e78b22bae7fb9ef1214f8976e7131
Nguyễn Trung Kiên <truggkien7902@gmail.com>
2024-09-11 17:56:28 +0700

    First commit

---
+++ PMS_BE/src/main/java/sep490/g13/pms_be/PmsBeApplication.java
@@ -0,0 +1,13 @@
+package sep490.g13.pms_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PmsBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PmsBeApplication.class, args);
+    }
+
+}
---
+++ PMS_BE/src/test/java/sep490/g13/pms_be/PmsBeApplicationTests.java
@@ -0,0 +1,13 @@
+package sep490.g13.pms_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PmsBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
