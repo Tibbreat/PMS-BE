3c041711e72a2182016503b4bb4c28ac8804b4e4
Nguyễn Trung Kiên <truggkien7902@gmail.com>
2024-09-11 17:45:30 +0700

    First commit

---
+++ SEP490_G13_BE/src/main/java/sep490/g13/Sep490G13BeApplication.java
@@ -0,0 +1,13 @@
+package sep490.g13;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sep490G13BeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sep490G13BeApplication.class, args);
+    }
+
+}
---
+++ SEP490_G13_BE/src/test/java/sep490/g13/Sep490G13BeApplicationTests.java
@@ -0,0 +1,13 @@
+package sep490.g13;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sep490G13BeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
