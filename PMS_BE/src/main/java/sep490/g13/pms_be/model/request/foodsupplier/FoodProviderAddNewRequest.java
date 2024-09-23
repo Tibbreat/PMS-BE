dee87733911c9cab29e8cc4d928855ee53c50573
QuanLH <quanlh1002@gmail.com>
2024-09-23 10:17:33 +0700

    Add DTO

--- PMS_BE/src/main/java/sep490/g13/pms_be/model/request/foodsupplier/FoodProviderAddNewRequest.java
+++ PMS_BE/src/main/java/sep490/g13/pms_be/model/request/foodsupplier/FoodProviderAddNewRequest.java
@@ -1,8 +1,25 @@
 package sep490.g13.pms_be.model.request.foodsupplier;
 
 
+import jakarta.persistence.Entity;
+import lombok.*;
 import sep490.g13.pms_be.entities.Auditable;
 
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class FoodProviderAddNewRequest {
 
+
+    private String representativeName;
+    private String providerName;
+    private String providerPhone;
+    private String providerEmail;
+    private String providerAddress;
+    private String providerRegisterNumber;
+    private String providerLicenseNumber;
+    private String contractFile;
+    private Boolean isActive;
 }
