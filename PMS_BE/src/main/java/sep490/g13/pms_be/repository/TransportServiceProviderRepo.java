df9850d388390684ac9f40b1edb8ff65fb6dbad3
Dũng <dungnvhe176203@fpt.edu.vn>
2024-10-01 09:49:11 +0700

    Merge Code From QuanLH

--- PMS_BE/src/main/java/sep490/g13/pms_be/repository/TransportServiceProviderRepo.java
+++ PMS_BE/src/main/java/sep490/g13/pms_be/repository/TransportServiceProviderRepo.java
@@ -3,8 +3,11 @@ package sep490.g13.pms_be.repository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 import sep490.g13.pms_be.entities.TransportServiceProvider;
-import sep490.g13.pms_be.model.request.foodsupplier.UpdateFoodProviderRequest;
 import sep490.g13.pms_be.model.request.transportsupplier.UpdateTransportRequest;
 
 public interface TransportServiceProviderRepo extends JpaRepository<TransportServiceProvider, String> {
